7d171b64d6132ac24280b1113442db6d5af6e3c1
Kassie Sanisclaws <bytecbdeveloping@gmail.com>
2021-09-29 12:26:37 -0400

    Fourth Commit

--- script.js
+++ script.js
@@ -1,56 +1,90 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // below is the query selector accessed by data type.//
 const gameCardTiles = document.querySelectorAll("[data-cards-tile]");
-const newGameButton = document.querySelector("[data-new-game]");
-const playerStartingScoreTextElement = document.querySelector("[data-total-clicks]");
+const resetGameButton = document.querySelector("[data-reset-game]");
+const playerScoreTextElement = document.querySelector("[data-total-clicks]");
 const playerGameTimerTextElement = document.querySelector("[data-game-timer]");
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // variables.//
-let isCardTileFlipped = false;  //Tracks if the card tile is flipped.//
-let firstCardChosen = [];       //First card tile choice stored here.//
-let secondCardChosen = [];      //Second card tile choice stored here.//
-let gameBoardDisabled = false;  //Tracks if the user has (2) being.//
-let totalClicks = 0;            //This is the score system based off least amount of clicks.//
-let gameTimer = 50;             //Game timer decrementing from 50 - 0 which signals game over.//
-let gameCards = Array.from(gameCardTiles);
+let isCardTileFlipped = false;                  //Tracks if the card tile is flipped.//
+let firstCardTileChosen, secondCardTileChosen;  //First card tile choice stored here.//+//Second card tile choice stored here.// 
+let gameBoardDisabled = false;                  //Tracks if the user has (2) being.//
+let totalClicks = 0;                            //This is the score system based off least amount of clicks.//
+let gameTimer = 50;                             //Game timer decrementing from 50 - 0 which signals game over.//
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Event listeners.//
-newGameButton.addEventListener("click", newGame);                             //New game button reset game shuffle cards.//
-gameCardTiles.forEach(gameCards => gameCards.addEventListener("click", flipCardTile));  //Listening for user click on card tile passing to the function.//
+resetGameButton.addEventListener("click", resetGameButton);   //New game button reset game shuffle cards.//
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class MemoryGameBoard {
+    constructor(gameCardTiles){
+        this.gameCardTiles = gameCardTiles;
+    }
+
 //Function for flipCardTile.//
-function flipCardTile(){
-// flip clicked card tile show to user.//s
-if(gameBoardDisabled) return;
+  flipCardTile(){
+    console.log("flipped")
+  }
 
-console.log("flipped")
+//Function for match card tile.//
+  cardTilesMatch(){
+    console.log("Match")
+}
 
+//Function for card tile shuffle.//
+  shuffleCardTiles(){
+     
+    console.log("shuffled")
+}
+ 
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const memoryGameBoard = new MemoryGameBoard(gameCardTiles)
+/////////////////////////////////////////////////////////////
+//GameCardTiles holding the array of the card tiles.//
+gameCardTiles.forEach(gameCards => {
+    gameCards.addEventListener("click", () => {
+        memoryGameBoard.flipCardTile(gameCards.innerText)
+    });
+});  
+////////////////////////////////////////////////////////////
+//Reset game board button.//
 
-// prevent clicked flipped card from register click function again.//
-
+resetGameButton.addEventListener("click", button => {
+         memoryGameBoard.shuffleCardTiles()
    
+});
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// class constructor to hold the memory game functions.//
+class MemoryGame {
+    constructor(playerScoreTextElement, playerGameTimerTextElement){
+         this.playerScoreTextElement = playerScoreTextElement;
+         this.playerGameTimerTextElement = playerGameTimerTextElement;
+         resetGame()
+    }
+
+resetGame(){
+     this.playerScoreTextElement = 0;
+     this.playerGameTimerTextElement = "";
+
+  }
+
+
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// new game function.//
- function newGame(){
-     
-     console.log('working');
-     
- }
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Function for card tile match.//
-function cardTilesMatch(){
-    
-}
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// functionshuffle card tiles.//
-function shuffleCardTiles(){
-      gameCardTiles.forEach(gameCards => {
-     let gameBoard = Math.floor(Math.random() * 16);
-         gameCards.style.order = gameBoard;
-      })
-}
+
+ 
+
+
+
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // this is the cardTiles function.//
 // gameCardTiles.forEach(card => {
@@ -73,9 +107,7 @@ function shuffleCardTiles(){
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
- 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 
 
