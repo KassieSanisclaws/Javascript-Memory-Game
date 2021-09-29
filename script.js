10a3bdbaed4d3b490e5edfa3056fde3c43a10214
Kassie Sanisclaws <bytecbdeveloping@gmail.com>
2021-09-29 17:13:54 -0400

    Seventh Commit

--- script.js
+++ script.js
@@ -55,26 +55,26 @@ function createGameBoard(){
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // this is the cardTiles function.//
-cardTiles.forEach(card => {
-card.addEventListener("click", () =>{
-    // this if statement is preventing the clicked active card tile, the prevent card click and card matchclassName.//
-   if(hasCardTileClick || card === clickedCardTile || card.className.includes("match")){
-       return
-   }else{
-    // this if statement shows the card tile that was clicked image.//
-       if(card === clickedCardTile && card.className.includes("match")){
-          card.className = card.className
-          .replace("img-hidden", "")
-          .trim();
-       }
-   }
-   // working on the below function.//
+// cardTiles.forEach(card => {
+// card.addEventListener("click", () =>{
+//     // this if statement is preventing the clicked active card tile, the prevent card click and card matchclassName.//
+//    if(hasCardTileClick || card === clickedCardTile || card.className.includes("match")){
+//        return
+//    }else{
+//     // this if statement shows the card tile that was clicked image.//
+//        if(card === clickedCardTile && card.className.includes("match")){
+//           card.className = card.className
+//           .replace("img-hidden", "")
+//           .trim();
+//        }
+//    }
+//    // working on the below function.//
    
-    card.className += "match";
+//     card.className += "match";
 
-    console.log(card.getAttribute("data-cards-tile"));
-})
-})
+//     console.log(card.getAttribute("data-cards-tile"));
+// })
+// })
 
 
 
